27052981d25710cf35519ed66c651bf108e70f5f
Mo Wong <mowong1987@gmail.com>
2020-09-28 09:21:32 +0200

    feat: building animation

--- app/components/RepList.js
+++ app/components/RepList.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { StyleSheet, Text, View, SafeAreaView, FlatList, TouchableOpacity, Image, Dimensions } from 'react-native';
+import React, { useRef } from 'react';
+import { StyleSheet, Text, View, SafeAreaView, FlatList, TouchableOpacity, Image, Dimensions, Animated } from 'react-native';
 import dem from '../../assets/dem.png';
 import gop from '../../assets/gop.png';
 const { width, height } = Dimensions.get('window');
@@ -8,11 +8,12 @@ export default function RepList ({ reps, navigation }) {
 
   const spacing = 20;
   const item_size = width * .9;
+  const scrollX = useRef(new Animated.Value(0)).current;
 
   return (
     <SafeAreaView style={styles.container}>
 
-      <FlatList
+      <Animated.FlatList
         showsHorizontalScrollIndicator={false}
         data={reps.slice(2, 5)}
         keyExtractor={rep => rep.name}
@@ -20,6 +21,14 @@ export default function RepList ({ reps, navigation }) {
         contentContainerStyle={{
           alignItems: 'center'
         }}
+        snapToInterval={item_size}
+        decelerationRate={0}
+        bounces={false}
+        onScroll={Animated.event(
+          [{ nativeEvent: { contentOffset: { x: scrollX } } }],
+          { useNativeDriver: true }
+        )}
+        scrollEventThrottle={15}
         renderItem={({item}) => {
           return (
           <View>
